bf643e136f4c766025b4c28c37ebd83914377825
Renan Martins <renanmartins989@gmail.com>
2023-02-01 03:20:05 +0000

    cloudinary working, cat crud done. Need to implement devise and create admin

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -26,6 +26,6 @@ class CategoriesController < ApplicationController
   private
 
   def category_params
-    params.require(:category).permit(:title)
+    params.require(:category).permit(:title, :photo)
   end
 end
