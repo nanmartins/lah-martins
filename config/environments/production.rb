113ff4b5594fe4452654f95683cfadc9b4e9e4cd
Renan Martins <renanmartins989@gmail.com>
2023-02-17 17:12:15 +0000

    error with MAIL variables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,7 +98,7 @@ Rails.application.configure do
     port:                 587,
     domain:               'gmail.com',
     user_name:            'MAIL_USERNAME',
-    password:             MAIL_PASSWORD,
+    password:             'MAIL_PASSWORD',
     authentication:       'plain',
     enable_starttls_auto: true
   }
