9508478051de2512a98d71ace568e2e54fbce438
Renan Martins <renanmartins989@gmail.com>
2023-02-18 01:53:39 +0000

    ...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,8 +97,8 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'gmail.com',
-    user_name:            MAIL_USERNAME,
-    password:             MAIL_PASSWORD,
+    user_name:            'MAIL_USERNAME',
+    password:             'MAIL_PASSWORD',
     authentication:       'plain',
     enable_starttls_auto: true
   }
